323e4ec263c49cc276bcf1470214e0c30b6819ea
CleverElsie <yubisiba@gmail.com>
2024-08-21 02:25:11 +0900

    revise Union Find and LIS

--- DP/LIS.cpp
+++ DP/LIS.cpp
@@ -1,6 +1,6 @@
 #include<vector>
 #include<algorithm>
-int LIS(std::vector<int>&a){
+std::vector<int> LIS(std::vector<int>&a){
 	int len=0;
 	std::vector<int>dp(a.size()),L(a.size()+1,1ll<<60);
 	std::vector<int>::iterator B=L.begin();
@@ -12,4 +12,18 @@ int LIS(std::vector<int>&a){
 			len++;
 	}
 	return len+1;
+}
+
+int LIS(std::vector<int>&a){
+	std::vector<int> dp(a.size()+1,1ll<<60);
+	dp[0]=0;
+	int ans=0;
+	for(int i=0;i<n;i++){
+		int k=lower_bound(A(dp),a[i])-dp.begin();
+		if(dp[k-1]<a[i]){
+			dp[k]=min(dp[k],a[i]);
+			ans=max(k,ans);
+		}
+	}
+	return ans;
 }
--- Graph/unionfind.cpp
+++ Graph/unionfind.cpp
@@ -3,19 +3,15 @@
 #include <vector>
 
 class union_find {
-	std::vector<int> root_array, size_array;
-	int root(int i) { return (i==root_array[i]?i:root_array[i]=root(root_array[i])); }
+	std::vector<int> par, rank;
+	int root(int i) { return (i==par[i]?i:par[i]=root(par[i])); }
 public:
-	union_find(int n) {
-		root_array.resize(n), size_array.resize(n,1);
-		std::iota(begin(root_array), end(root_array), 0);
-	}
+	union_find(int n) { par.resize(n), rank.resize(n,0), std::iota(begin(par), end(par), 0); }
 	bool same(int i, int j) { return root(i) == root(j); }
 	void unite(int i, int j) {
 		i=root(i),j=root(j);
-		if(size_array[i]>size_array[j])std::swap(i,j);
-		root_array[i]=j;
-		size_array[j]+=size_array[i];
+		if(i==j)return;
+		rank[i]+=rank[i]==rank[j];
+		if(rank[i]<rank[j])par[i]=j;else par[j]=i;
 	}
-	int size(int i) { return size_array[root(i)]; }
 };
