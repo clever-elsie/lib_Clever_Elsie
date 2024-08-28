6ea21687c758d2d655a62bc68ff4780a3490feb0
CleverElsie <yubisiba@gmail.com>
2024-08-29 00:14:40 +0900

    add adjacency matrix dijkstra

--- Graph/dijkstra.cpp
+++ Graph/dijkstra.cpp
@@ -3,19 +3,22 @@
 #include <vector>
 
 // weighted graph's dijkstra
-using namespace std;
-template<class t>
-using vc = vector<t>;
-template<class t>
-using vv = vc<vc<t>>;
-using vi = vector<int>;
-using vb = vector<bool>;
-using pi = pair<int,int>;
-vi dijkstra(vv<pi> &edge,int start=0) {
-	vi dist(edge.size(), 1ll << 60);
-	priority_queue<pi,vc<pi>,greater<pi>>q;
+std::vector<int>
+dijkstra(
+	std::vector<
+		std::vector<
+			std::pair<int,int>>> &edge,
+	int start=0
+) {
+	std::vector<int>
+		dist(edge.size(),
+			(sizeof(int)==4?1<<30:1ll<<60));
+	std::priority_queue<
+		std::pair<int, int>,
+		std::vector<std::pair<int, int>>,
+		std::greater<std::pair<int, int>>> q;
 	dist[start] = 0;
-	q.push(make_pair(dist[start], start));
+	q.push(std::make_pair(dist[start], start));
 	while (!q.empty()) {
 		auto [now_dist, here] = q.top();
 		q.pop();
@@ -24,6 +27,34 @@ vi dijkstra(vv<pi> &edge,int start=0) {
 			if (e_cost + now_dist < dist[to]) {
 				dist[to] = e_cost + now_dist;
 				q.push(std::make_pair(dist[to], to));
+			}
+		}
+	}
+	return dist;
+}
+
+std::vector<int>
+dijkstra(
+	std::vector<
+		std::vector<int>>&edge,
+	int start=0;
+){
+	std::priority_queue<
+		std::pair<int,int>,
+		std::vector<std::pair<int,int>>,
+		std::greater<std::pair<int,int>>>q;
+	std::vector<int>dist(edge.size(),(sizeof(int)==4?1<<30:1ll<<60));
+	dist[start]=0;
+	q.push(std::make_pair(dist[start],start));
+	while(q.size()){
+		auto [cost,here]=q.top();
+		q.pop();
+		if(cost>dist[here])continue;
+		for(int to=0;to<edge.size();to++){
+			int next_cost=dist[here]+edge[here][to];
+			if(next_cost<dist[to]){
+				dist[to]=next_cost;
+				q.push(std::make_pair(dist[to],to));
 			}
 		}
 	}
