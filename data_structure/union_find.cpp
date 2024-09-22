6ba076f446ad1ec952bc9c2c2770183d44912106
CleverElsie <yubisiba@gmail.com>
2024-09-22 23:32:38 +0900

    add public root() in union_find

--- data_structure/union_find.cpp
+++ data_structure/union_find.cpp
@@ -4,9 +4,9 @@
 
 class union_find {
 	std::vector<int> par, rank;
-	int root(int i) { return (i==par[i]?i:par[i]=root(par[i])); }
 public:
 	union_find(int n) { par.resize(n), rank.resize(n,0), std::iota(begin(par), end(par), 0); }
+	int root(int i) { return (i==par[i]?i:par[i]=root(par[i])); }
 	bool same(int i, int j) { return root(i) == root(j); }
 	void unite(int i, int j) {
 		i=root(i),j=root(j);
