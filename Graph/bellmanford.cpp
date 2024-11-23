78e143a4d46c778975ffd9931f15098497aaebf8
CleverElsie <yubisiba@gmail.com>
2024-11-23 22:42:51 +0900

    fix bellmanford 	before 		7:e[start]=0; 	after 		7:dist[start]=0;
    	change edge type
    	before [cost,[from,to]]
    	after [cost,to]

--- Graph/bellmanford.cpp
+++ Graph/bellmanford.cpp
@@ -3,23 +3,23 @@
 #include<cstdint>
 using namespace std;
 
-vector<int64_t> BF(vector<vector<pair<int64_t,pair<int,int>>>>&e,int64_t start=0) noexcept(false) {
+vector<int64_t> BF(vector<vector<pair<int64_t,int>>>&e,int64_t start=0) noexcept(false) {
 	vector<int64_t> dist(e.size(),INT64_MAX);
-	e[start]=0;
+	dist[start]=0;
 	for(int _=1;_<e.size();_++){
 		for(int i=0;i<e.size();i++){
 			for(int j=0;j<e[i].size();j++){
-				if(dist[e[i][j].second.first]<INT64_MAX){
-					if(dist[e[i][j].second.first]+e[i][j].first<dist[e[i][j].second.second])
-						dist[e[i][j].second.second]=dist[e[i][j].second.first]+e[i][j].first;
+				if(dist[i]<INT64_MAX){
+					if(dist[i]+e[i][j].first<dist[e[i][j].second])
+						dist[e[i][j].second]=dist[i]+e[i][j].first;
 				}
 			}
 		}
 	}
 	for(int i=0;i<e.size();i++){
 		for(int j=0;j<e[i].size();j++){
-			if(dist[e[i][j].second.first]<INT64_MAX){
-				if(dist[e[i][j].second.first]+e[i][j].first<dist[e[i][j].second.second])
+			if(dist[i]<INT64_MAX){
+				if(dist[i]+e[i][j].first<dist[e[i][j].second])
 					throw (int64_t)(-1);
 			}
 		}
