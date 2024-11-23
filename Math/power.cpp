624565cd56d848b1d7690d13c2f7f60732901f72
CleverElsie <yubisiba@gmail.com>
2024-11-23 20:16:11 +0900

    add safe power and mod power. erase std:: in matrix power

--- Math/power.cpp
+++ Math/power.cpp
@@ -1,25 +1,58 @@
 #include<vector>
+#include<cstdint>
+#include<limits>
 
-template <class f>
-std::vector<std::vector<f>> MP(std::vector<std::vector<f>> &A, int b) {
-	auto mul = [&](std::vector<std::vector<f>> &a, std::vector<std::vector<f>> &b) -> std::vector<std::vector<f>> {
-		size_t rank = a.size();
-		std::vector<std::vector<f>> res(rank, std::vector<f>(rank, 0));
-		for (size_t i = 0; i < rank; i++)
-			for (size_t j = 0; j < rank; j++)
-				for (int k = 0; k < rank; k++)
-					res[i][j] += a[i][k] * b[k][j];
+int64_t safepow(int64_t a,uint64_t b){
+	int64_t ret=1;
+	int64_t k=a;
+	double check=1,kcheck=a;
+	while(b){
+		if(b&1){
+			check*=k;
+			if(check>INT64_MAX)return INT64_MAX;
+			if(check<INT64_MIN)return INT64_MIN;
+			ret*=k;
+		}
+		kcheck*=k;
+		if(kcheck>INT64_MAX)return INT64_MAX;
+		if(kcheck<INT64_MIN)return INT64_MIN;
+		k*=k;
+		b>>=1;
+	}
+	return ret;
+}
+
+int64_t modpow(int64_t a,uint64_t b,uint64_t mod){
+	__uint128_t k=a%mod,ret=1;
+	if(k<0)k+=mod;
+	while(b){
+		if(b&1)ret=(ret*k)%mod;
+		k=(k*k)%mod;
+		b>>=1;
+	}
+	return int64_t(ret);
+}
+
+template<class f>
+vector<vector<f>>Mpower(vector<vector<f>>&A,uint64_t b) {
+	auto mul=[&](vector<vector<f>>&a,vector<vector<f>>&b)->vector<vector<f>> {
+		size_t rank=a.size();
+		vector<vector<f>>res(rank,vector<f>(rank,0));
+		for(size_t i=0;i<rank;i++)
+		for(size_t j=0;j<rank;j++)
+		for(size_t k=0;k<rank;k++)
+			res[i][j]+=a[i][k]*b[k][j];
 		return res;
 	};
-	size_t rank = A.size();
-	std::vector<std::vector<f>> I = A;
-	std::vector<std::vector<f>> res(rank, std::vector<f>(rank, 0));
-	for (int i = 0; i < rank; i++)
-		res[i][i] = 1;
-	while (b) {
-		if (b & 1) res = mul(res, I);
-		I = mul(I, I);
-		b >>= 1;
+	size_t rank=A.size();
+	vector<vector<f>>I=A;
+	vector<vector<f>>res(rank,vector<f>(rank,0));
+	for(int i=0;i<rank;i++)
+		res[i][i]=1;
+	while(b){
+		if(b&1)res=mul(res,I);
+		I=mul(I,I);
+		b>>=1;
 	}
 	return res;
 }
