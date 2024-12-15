e8b1b2d0ab80da226f7426ccfbcec270d32726e3
CleverElsie <yubisiba@gmail.com>
2024-12-15 14:52:09 +0900

    make const argument in inversion_number and make shorter

--- tree/inversion_number.cpp
+++ tree/inversion_number.cpp
@@ -1,19 +1,22 @@
 #include <algorithm>
 #include <atcoder/fenwicktree>
 #include <vector>
+using namespace std;
+using namespace atcoder;
 
-template <class f> int inversion_number(std::vector<f> &target) {
-	std::vector<f> arr = target;
-	std::vector<f> press = target;
-	std::sort(press.begin(), press.end());
-	press.erase(unique(press.begin(), press.end()), press.end());
-	for (auto &x : arr)
-		x = std::lower_bound(press.begin(), press.end(), x) - press.begin();
-	atcoder::fenwick_tree<f> bit(press.size());
-	int res = 0;
-	for (int i = 0; i < arr.size(); i++) {
-		res += i - bit.sum(0, arr[i]);
-		bit.add(arr[i], 1);
+template<class f>
+int inversion_number(const vector<f>&tar){
+	int res=0;
+	const size_t n=tar.size();
+	vector<f>arr=tar, press=tar;
+	fenwick_tree<f>bit(n);
+	auto prb=press.begin(), pre=press.end();
+	sort(prb,pre);
+	press.erase(unique(prb,pre),pre);
+	for(auto&x:arr)x=lower_bound(prb,pre,x)-prb;
+	for (int i=0;i<n;i++){
+		res+=i-bit.sum(0,arr[i]);
+		bit.add(arr[i],1);
 	}
 	return res;
 }
