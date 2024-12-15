301acd06f029c169ce753a678ff6eb62d551f0f7
CleverElsie <yubisiba@gmail.com>
2024-12-15 15:52:23 +0900

    rolling hash verified by AOJ

--- String/rollingHash.cpp
+++ String/rollingHash.cpp
@@ -9,33 +9,42 @@ using namespace std;
 
 class rollingHash{
 	using ll = long long;
-	constexpr static ll psz = 6;
-	constexpr static ll mods[psz]={998244353,998244383,(ll)(1e9+7),(ll)(1e9+9),(ll)(1e9+21),(ll)(1e9+33)};
+	using vl = vector<ll>;
+	using str = string;
+	constexpr static ll psz = 4;
+	constexpr static ll mods[psz]={998244353,998244383,(ll)(1e9+7),(ll)((1LL<<61)-1)};
 	ll b[psz]={0};
 	array<array<ll,256>,psz>trans;
-	array<vector<ll>,psz>hash,thash;
-	string src;
-	ll spow(int i,int p){
-		ll r=1,s=b[i];
-		while(p){
-			if(p&1) r=(r*s)%mods[i];
-			p>>=1;
-			s*=s;
-		}
-		return r;
+	array<vl,psz>hash,thash;
+	str src;
+	ll mul(ll a,ll b,ll mod){
+		__int128_t s=a;
+		s=(s*b)%mod;
+		return s;
 	}
-	void make_hash(const string&s,array<vector<ll>,psz>&h){
+	ll modpow(ll a,ll b,ll mod){
+		__uint128_t k=a%mod,ret=1;
+		if(k<0)k+=mod;
+		while(b){
+			if(b&1)ret=(ret*k)%mod;
+			k=(k*k)%mod;
+			b>>=1;
+		}
+		return ll(ret);
+	}
+	ll spow(ll i,ll p){ return modpow(b[i],p,mods[i]); }
+	void make_hash(const str&s,array<vl,psz>&h){
 		for(int i=0;i<psz;i++){
 			h[i].resize(s.size()+1,0ull);
 			for(int j=0;j<s.size();j++){
-				h[i][j+1]=(h[i][j]*b[i]+trans[i][s[j]])%mods[i];
+				h[i][j+1]=(mul(h[i][j],b[i],mods[i])+trans[i][s[j]])%mods[i];
 				if(h[i][j+1]<0)h[i][j+1]+=mods[i];
 			}
 		}
 	}
 	void make_rand(){
 		random_device seed;
-		mt19937 gen(seed());
+		mt19937_64 gen(seed());
 		for(int i=0;i<psz;i++){
 			while(b[i]<1e3) b[i]=gen()%mods[i];
 			for(int j=0;j<256;j++)
@@ -43,33 +52,21 @@ class rollingHash{
 		}
 	}
 	public:
-	rollingHash(const string&SRC):src(SRC){
-		make_rand();
-		make_hash(src,hash);
-	}
-	rollingHash(string &&SRC){
-		src=move(SRC);
-		make_rand();
-		make_hash(src,hash);
-	}
-	void set(const string&SRC){
-		src=SRC;
-		make_hash(src,hash);
-	}
-	void set(string&&SRC){
-		src=move(SRC);
-		make_hash(src,hash);
-	}
+	rollingHash(const string&SRC):src(SRC){make_rand(),make_hash(src,hash);}
+	rollingHash(string &&SRC):src(SRC){make_rand(),make_hash(src,hash);}
+	void set(const string&SRC){src=SRC,make_hash(src,hash);}
+	void set(string&&SRC){src=move(SRC),make_hash(src,hash);}
+
 	vector<size_t>find(const string&tar){
 		vector<size_t> ret;
 		make_hash(tar,thash);
 		array<ll,psz>h;
 		const size_t len = tar.size();
 		for(int i=0;i<psz;i++)h[i]=thash[i].back();
-		for(size_t j=len;j<src.size()+1;j++){
+		for(ll j=len;j<src.size()+1;j++){
 			bool all=true;
 			for(int i=0;i<psz;i++){
-				int key=(hash[i][j]-hash[i][j-len]*spow(i,len)%mods[i])%mods[i];
+				ll key=(hash[i][j]-mul(hash[i][j-len],spow(i,len),mods[i]))%mods[i];
 				if(key<0)key+=mods[i];
 				if(key!=h[i]){
 					all=false;
@@ -79,13 +76,5 @@ class rollingHash{
 			if(all) ret.push_back(j-len);
 		}
 		return move(ret);
-	}
-	void putv(){
-		for(int i=0;i<psz;i++){
-			for(int j=0;j<thash[i].size();j++)
-				cout<<thash[i][j]<<" \n"[j==thash[i].size()-1];
-			for(int j=0;j<hash[i].size();j++)
-				cout<<hash[i][j]<<" \n"[j==hash[i].size()-1];
-		}
 	}
 };
