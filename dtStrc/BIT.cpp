05419160d41968560ddcf79a27a01c80cbebc3c9
CleverElsie <yubisiba@gmail.com>
2025-01-10 18:10:37 +0900

    make BIT shorter

--- dtStrc/BIT.cpp
+++ dtStrc/BIT.cpp
@@ -7,30 +7,17 @@ class BIT{ // 0-indexed [l,r)
 	size_t _size;
 	type sum(int64_t r){
 		type ret=0;
-		while(r){
-			ret+=v[r-1];
-			r-=r&-r;
-		}
+		for(;r;r-=r&-r) ret+=v[r-1];
 		return ret;
 	}
 public:
 	BIT(size_t size){
-		assert(0<=size&&size<=1'000'000'000);
 		size_t n=1;
 		while(n<size)n*=2;
 		_size=n, v.resize(n);
 	}
-	void add(size_t i,type val){
-		i++;
-		do{
-			v[i-1]+=val;
-			i+=i&-i;
-		}while(i<=_size);
-	}
+	void add(size_t i,type val){ for(i++;i<=_size;i+=i&-i) v[i-1]+=val; }
 	type sum(size_t l,size_t r){ return sum(r)-sum(l); }
 	type get(size_t i){ return sum(i,i+1); }
-	void set(size_t i,type t){
-		type now=get(i);
-		add(i,t-now);
-	}
+	void set(size_t i,type t){ add(i,t-get(i)); }
 };
