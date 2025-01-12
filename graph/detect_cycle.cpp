958fc053ec3721575993ef4cb2ebc81a7a86ab72
CleverElsie <yubisiba@gmail.com>
2025-01-12 19:09:37 +0900

    fix detect_cycle

--- graph/detect_cycle.cpp
+++ graph/detect_cycle.cpp
@@ -5,15 +5,12 @@
 #include <algorithm>
 #include <cstddef>
 using namespace std;
-template<class f> using vc=vector<f>;
-template<class f> using vv=vc<vc<f>>;
-using vi = vc<int>;
-using vb = vc<bool>;
-vi find_cycle_directed(const vv<int>&e){
+
+vector<int> find_cycle_directed(const vector<vector<int>>&e){
 	size_t n=e.size();
-	vb seen(n,false);
+	vector<bool> seen(n,false);
 	set<int>now_path;
-	vi cycle(0);cycle.reserve(n);
+	vector<int> cycle(0);cycle.reserve(n);
 	function<int(int)> dfs=[&](int now)->int {
 		for(const auto&to:e[now]){
 			if(now_path.contains(to)){
@@ -45,8 +42,8 @@ vi find_cycle_directed(const vv<int>&e){
 	return move(cycle);
 }
 
-vi reconstruct_edge_id_from_to(const vi&v,const vc<map<int,int>>&e_id){
-	vi ans(v.size()-1);
+vector<int> reconstruct_edge_id_from_to(const vector<int>&v,const vc<map<int,int>>&e_id){
+	vector<int> ans(v.size()-1);
 	for(size_t i=0;i+1<v.size();i++){
 		auto itr=e_id[v[i]].find(v[i+1]);
 		ans[i]=itr->second;
