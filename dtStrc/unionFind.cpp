96e23ff598c7056d546b4e61ae3874c0cd98bc06
CleverElsie <yubisiba@gmail.com>
2025-01-17 22:26:31 +0900

    add operator= at unionFind

--- dtStrc/unionFind.cpp
+++ dtStrc/unionFind.cpp
@@ -6,20 +6,35 @@
 using namespace std;
 
 class unionFind{
-	private:const int _order;
-	protected:vector<int>pr;
+	protected:
+		using it=int32_t;
+		it _order;
+		mutable vector<it>pr;
 	public:
-		unionFind(int n):_order(n),pr(n,-1){}
-		int ord(){return _order;}
-		int size(int u){return -pr[root(u)];}
-		int root(int u){return(pr[u]<0?u:pr[u]=root(pr[u]));}
-		bool same(int u,int v){return root(u)==root(v);}
-		void unite(int u,int v) {
+		unionFind():_order(0),pr(0){}
+		unionFind(it n):_order(n),pr(n,-1){}
+		it ord()const{return _order;}
+		it size(it u)const{return -pr[root(u)];}
+		it root(it u)const{return(pr[u]<0?u:pr[u]=root(pr[u]));}
+		bool same(it u,it v)const{return root(u)==root(v);}
+		void unite(it u,it v)const{
 			u=root(u),v=root(v);
 			if(u==v)return;
 			if(pr[u]<pr[v])swap(u,v);
 			pr[v]+=pr[u];
 			pr[u]=v;
+		}
+		unionFind&operator=(const unionFind&other){
+			if(this!=&other){
+				_order=other._order;
+				pr=other.pr;
+			} return*this;
+		}
+		unionFind&operator=(unionFind&&other){
+			if(this!=&other){
+				_order=other._order;
+				pr=move(other.pr);
+			} return*this;
 		}
 };
 
