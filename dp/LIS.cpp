c89980b508124eee199ec44433b1f20b00fd3b67
CleverElsie <yubisiba@gmail.com>
2025-01-25 18:25:31 +0900

    fix small bug on LIS.

--- dp/LIS.cpp
+++ dp/LIS.cpp
@@ -5,9 +5,9 @@ using namespace std;
 template<class T>
 int LIS(const vector<T>&a){
 	vector<int> dp(a.size()+1,1ll<<60);
-	dp[0]=0;
+	dp[0]=-(1ll<<60);
 	int ans=0;
-	for(int i=0;i<n;i++){
+	for(int i=0;i<a.size();i++){
 		int k=lower_bound(begin(dp),end(dp),a[i])-begin(dp);
 		if(dp[k-1]<a[i]){
 			dp[k]=min(dp[k],a[i]);
