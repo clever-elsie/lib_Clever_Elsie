81a6cf267a7bb4f524baac47726563d135dab707
CleverElsie <yubisiba@gmail.com>
2025-02-07 16:54:53 +0900

    めんどいけどユーザーインタフェースのバグ取りを頑張ってくれ

--- dtStrc/tree.hpp
+++ dtStrc/tree.hpp
@@ -11,72 +11,42 @@
 #define ELSIE_RBTREE
 namespace elsie{
 using namespace std;
-	namespace rbtree_internal{
-		template<class key_t,class val_t,class lazy_t,class cmp,bool allow_duplicate_keys=std::false>
-		struct rbnode{
-			using ip=rbnode*;
-			ip l,r,p,nx,pr;
-			uint64_t time,size; // time: odd red, even black, second bit is reverse flag
-			const key_t key;
-			val_t val,data;
-			lazy_t lz;
-			rbnode(ip nil):key(),val(),lz(),time(0),size(1),
-				l(nil),r(nil),p(nil),nx(nil),pr(nil){}
-			rbnode(const key_t&k,const val_t&v,const lazy_t&Lz,int32_t tm,
-				ip P,ip L,ip R,ip Pr,ip Nx)
-				:key(k),val(v),lz(Lz),
-				time(tm),size(0),
-				p(P),l(L),r(R),pr(Pr),nx(Nx){}
-			auto operator<=>(const rbnode&other){
-				bool L=cmp(key,other.key),R=cmp(other.key,key);
-				if(L==R)return time<=>other.time;
-				return L?strong_ordering::less:strong_ordering::greater;
-			}
-		};
-		template<class key_t,class val_t,class lazy_t,class cmp,bool allow_duplicate_keys=std::false>
-		struct rbiterator{
-			rbiterator(rbnode*node):n(node){}
-			key_t&first;
-			val_t&second;
-			private:rbnode*n;
-		};
-		template<class key_t,class val_t,class lazy_t,class cmp,bool allow_duplicate_keys=std::false>
-		struct rbreverseiterator{
-			rbreverseiterator(rbnode*node):n(node){}
-			key_t&first;
-			val_t&second;
-			private:rbnode*n;
-		};
-	}
 
 struct null_t{ null_t(){} };
 
-template<class key_t,class val_t,class op,class e,
-	class lazy_t,class mapping,
-	class composition,class id,class cmp=less<key_t>,bool allow_duplicate_keys=false>
-class master_rbtree{
+template<class key_t,class val_t,class cmp=less<key_t>,bool allow_duplicate_keys=false>
+class rbtree{
 	static_assert(!is_same_v<key_t,null_t>);
-	static_assert(is_same_v<val_t,null_t>||(is_convertible_v(decltype(op),function<val_t(val_t,val_t)>)&&is_convertible_v(decltype(e),function<val_t()>)));
-	static_assert(is_same_v<lazy_t,null_t>||(is_convertible_v(decltype(mapping),function<val_t(lazy_t,val_t)>)&&is_convertible_v(decltype(composition),function<lazy_t(lazy_t,lazy_t)>)&&is_convertible_v(decltype(id),function<lazy_t()>)));
-
+	protected:
+	struct node{
+		using ip=node*;
+		ip l,r,p;
+		uint64_t time,size; // time: odd red, even black, second bit is reverse flag
+		const key_t key;
+		val_t val;
+		node(ip nil):key(),val(),time(0),size(1),l(nil),r(nil),p(nil){}
+		node(key_t&&k,val_t&&v,int32_t tm,ip P,ip L,ip R):key(move(k)),val(move(v)),time(tm),size(0),p(P),l(L),r(R){}
+		node(key_t&&k,const val_t&v,int32_t tm,ip P,ip L,ip R):key(move(k)),val(v),time(tm),size(0),p(P),l(L),r(R){}
+		node(const key_t&k,val_t&&v,int32_t tm,ip P,ip L,ip R):key(k),val(move(v)),time(tm),size(0),p(P),l(L),r(R){}
+		node(const key_t&k,const val_t&v,int32_t tm,ip P,ip L,ip R):key(k),val(v),time(tm),size(0),p(P),l(L),r(R){}
+		auto operator<=>(const node&other){
+			bool L=cmp(key,other.key),R=cmp(other.key,key);
+			if(L==R)return time<=>other.time;
+			return L?strong_ordering::less:strong_ordering::greater;
+		}
+	};
+	using np=node*;
+	using vp=pair<key_t,val_t>;
 	public:
 	using value_type=pair<const key_t,val_t>;
-	using iterator=rbtree_internal::rbiterator<key_t,val_t,lazy_t,cmp,allow_duplicate_keys>;
-	using reverse_iterator=rbtree_internal::rbreverseiterator<key_t,val_t,lazy_t,cmp,allow_duplicate_keys>;
 
 	protected:
-	const static uint64_t filter_red=1;
-	const static uint64_t filter_black=0xFFFF'FFFF'FFFF'FFFEul;
-	const static uint64_t filter_reverse=2;
-	using node=rbtree_internal::rbnode<key_t,val_t,lazy_t,cmp,allow_duplicate_keys>;
-	using np=node*;
+	constexpr static uint64_t filter_red=1;
+	constexpr static uint64_t filter_black=0xFFFF'FFFF'FFFF'FFFEul;
+	constexpr static uint64_t unit_time=2;
 	size_t cur_size,time;
 	np root,bs,es,nil;
-	/* 実装完了時に消すコメント
-		lower_boundを探索の基本として使う
-		lower_bound({key,0})がfind及びlower_bound
-		lower_bound({key,UINT64_MAX})がupper_bound
-		findではkeyが異なるときに落とす処理を加える． */
+
 	np lower_bound(const node&tar){
 		np cur=root,res=nil;
 		while(cur!=nil){
@@ -88,12 +58,30 @@ class master_rbtree{
 		}
 		return cur;
 	}
-	np find(np p){
-		np f=lower_bound(*p);
-		return f!=nil&&*f==*p?f:nil;
+	np ordered_access(size_t idx){
+		np cur=root;
+		while(cur!=nil){
+			size_t L=1;
+			if(cur->l!=nil)
+				L+=cur->l;
+			if(L==idx)return cur;
+			if(L>idx)cur=cur->l;
+			else {
+				cur=cur->r;
+				idx-=L;
+			}
+		}
+		return nil;
 	}
-	np ordered_access(int64_t idx){
+	size_t order_of_node(np p){
+		size_t R=0;
+		while(p!=nil){
+			R+=1+(p->l!=nil?p->l->size:0);
+			p=p->p;
+		}
+		return R;
 	}
+	public:
 	np minimum(np p){
 		for(;p->l!=nil;p=p->l);
 		return p;
@@ -102,65 +90,49 @@ class master_rbtree{
 		for(;p->r!=nil;p=p->r);
 		return p;
 	}
-	// nodeの中の属性を書き換える必要がある
-	void node_apply(np x,lazy_t*f){
-		x->val=mapping(*f,x->val);
-		x->data=mapping(*f,x->data);
-		x->lz=composition(*f,x->lz);
+	np next(np x){
+		if(x->r!=nil)return maximum(x->r);
+		while(1){
+			if(x==root)return nil;
+			np y=x;
+			x=x->p;
+			if(x->l==y)break;
+		}
+		return x;
 	}
-	void push_apply(np x){
-		node_apply(x,&x->lz);
-		if(x->l!=nil) node_apply(x->l,&x->lz);
-		if(x->r!=nil) node_apply(x->r,&x->lz);
-		x->lz=id();
+	np prev(np x){
+		if(x->l!=nil)return minimum(x->l);
+		while(1){
+			if(x==root)return nil;
+			np y=x;
+			x=x->p;
+			if(x->r==y)break;
+		}
 	}
-	void update_prod(np x){
-		x->data=x->val;
-		if(x->l!=nil)x->data=op(x->l->data,x->data);
-		if(x->r!=nil)x->data=op(x->data,x->r->data);
-	}
+	protected:
+	void calc_size(np x){ x->size=1+(x->l!=nil?x->l->size:0)+(x->r!=nil?x->r->size:0); }
 	np left_rotation(np x){
 		np y=x->r;
-		if constexpr(!is_same_v<lazy_t,null_t>)
-			push_apply(x),push_apply(y);
 		x->r=y->l,y->p=x->p;
 		if(y->l!=nil)y->l->p=x;
 		if(x->p==nil)root=y;
 		else if(x==x->p->l)x->p->l=y;
 		else x->p->r=y;
 		y->l=x,x->p=y;
-		swap(x->size,y->size);
-		if constexpr(!is_same_v<val_t,null_t>)
-			update_prod(x),update_prod(y);
+		calc_size(x);
+		calc_size(y);
 	}
 	np right_rotation(np x){
 		np y=x->l;
-		if constexpr(!is_same_v<lazy_t,null_t>)
-			push_apply(x),push_apply(y);
 		x->l=y->r,y->p=x->p;
 		if(y->r!=nil)y->r->p=x;
 		if(x->p==nil)root=y;
 		else if(x==x->p->l)x->p->l=y;
 		else x->p->r=y;
 		y->r=x,x->p=y;
-		swap(x->size,y->size);
-		if constexpr(!is_same_v<val_t,null_t>)
-			update_prod(x),update_prod(y);
+		calc_size(x);
+		calc_size(y);
 	}
-	void resolve_delay(np z){
-		vector<np>pst;
-		for(np u=z;u!=nil;u=u->p)
-			pst.push_back(u);
-		for(int32_t i=(int32_t)pst.size()-1;i>=0;--i)
-			push_apply(pst[i]);
-		for(int32_t i=0;i<(int32_t)pst.size();++i)
-			update_prod(pst[i]);
-	}
-	void ascend_prod(np z){
-		for(;z!=nil;z=z->p)
-			update_prod(z);
-	}
-	// ここまで
 	void rb_insert_fixup(np z){
 		while(z->p->time&filter_red){
 			if(z->p==z->p->p->l){
@@ -192,28 +164,22 @@ class master_rbtree{
 			if(*z<*x)x=x->l;
 			else x=x->r;
 		}
-		if constexpr(!is_same_v<lazy_t,null_t>)
-			resolve_delay(y);
 		if constexpr(!allow_duplicate_keys)
 			if(cmp(y->key,z->key)==cmp(z->key,y->key)){
-				if constexpr(!is_same_v<val_t,null_t>){
+				if constexpr(!is_same_v<val_t,null_t>)
 					y->val=z->val;
-					ascend_prod(y);
-				}
 				return y;
 			}
 		z->p=y;
 		if(y==nil){
-			bs->nx=y,y->pr=bs,y->nx=es,es->pr=y;
+			root=y;
 		}else if(*z<*y) y->l=z;
 		else y->r=z;
 		rb_insert_fixup(z);
-		if constexpr(!is_same_v<val_t,null_t>)
-			ascend_prod(z);
 		return z;
 	}
 
-	np rb_delete_fixup(np x){
+	void rb_delete_fixup(np x){
 		while(x!=root&&(x->time&filter_red)==0){
 			if(x==x->p->l){ // x is left child
 				np w=x->p->r;
@@ -262,100 +228,255 @@ class master_rbtree{
 		x->time&=filter_black;
 	}
 
-	np erase(np z){
-		auto transplant=[](np u,np v){
+	void erase(np z){
+		auto transplant=[&](np u,np v){
 			if(u->p==nil)root=v;
 			else if(u==u->p->l)u->p->l=v;
 			else u->p->r=v;
 			v->p=u->p;
+			calc_size(v);
+			calc_size(v->p);
 		};
-		np x,y=z;
+		np x,y=z,w=nil,v=nil;
 		bool y_was_red=y->time&filter_red;
 		if(z->l==nil){
-			x=z->r;
-			transplant(z,z->r);
+			transplant(z,x=z->r);
 		}else if(z->r==nil){
-			x=z->l;
-			transplant(z,z->l);
+			transplant(z,x=z->l);
 		}else{
-			y=minimum(z->r);
+			for(y=y->r;y->l!=nil;y=y->l)
+				y->size-=1;
 			y_was_red=y->time&filter_red;
 			x=y->r;
 			if(y!=z->r){
+				w=y->p;
 				transplant(y,y->r);
 				y->r=z->r,y->r->p=y;
 			}else x->p=y;
 			transplant(z,y);
 			y->l=z->l;
 			y->l->p=y;
+			v=y;
 			if(z->time&filter_red)y->time|=filter_red;
 			else y->time&=filter_black;
 		}
+		for(np s=x;1;s=s->p){
+			calc_size(s);
+			if(s==root)break;
+		}
 		if(!y_was_red)rb_delete_fixup(x);
+		if(z->p!=nil) z->p->size-=1;
 		delete z;
+	}
+	public://iterator
+	struct iterator{
+		iterator(rbtree*t,np node):tree(t),n(node){}
+		iterator&operator=(const iterator&itr){
+			n=itr.n;
+			tree=itr.tree;
+			return *this;
+		}
+		iterator&operator++(){
+			this->n=this->tree->next(this->n);
+			return *this;
+		}
+		iterator operator++(int32_t){
+			iterator cur=*this;
+			this->n=this->tree->next(this->n);
+			return cur;
+		}
+		iterator&operator--(){
+			this->n=this->tree->prev(this->n);
+			return*this;
+		}
+		iterator operator--(int32_t){
+			iterator cur=*this;
+			this->n=this->tree->prev(this->n);
+			return cur;
+		}
+		auto operator*(){
+			if constexpr(is_same_v<val_t,null_t>)
+				return (const key_t&)n->key;
+			else return pair<const key_t&,val_t&>(n->key,n->val);
+		}
+		bool operator==(const iterator&itr){ return this->n==itr.n; }
+		bool operator!=(const iterator&itr){ return this->n!=itr.n; }
+		const key_t&f(){return n->key;}
+		val_t&s(){return n->val;}
+		protected:
+		np n;
+		rbtree*tree;
+	};
+
+	struct reverse_iterator:public iterator{
+		reverse_iterator(rbtree*t,np node):iterator(t,node){}
+		iterator&operator++(){ return *this=this->tree->prev(this->n); }
+		iterator operator++(int32_t){
+			iterator cur=*this;
+			*this=this->tree->prev(this->n);
+			return cur;
+		}
+		iterator&operator--(){ return *this=this->tree->next(this->n); }
+		iterator operator--(int32_t){
+			iterator cur=*this;
+			*this=this->tree->next(this->n);
+			return cur;
+		}
+	};
+	protected:// wrapper
+	// auto -> const value_type& or value_type&&
+	iterator insert_wrapper(auto&&x){
+		time+=unit_time;
+		cur_size+=1;
+		if constexpr(allow_duplicate_keys)
+			return iterator(this,insert(new node(forward(x.first),forward(x.second),time|filter_red,nil,nil,nil)));
+		else{
+			node p=new node(forward(x.first),forward(x.second),0,nil,nil,nil);
+			np lb=lower_bound(&p);
+			if(cmp(x.first,lb->key)==cmp(lb->key,x.first)){
+				cur_size-=1;
+				lb->val=move(p->val);
+				delete p;
+			}else{
+				p->time=time|filter_red;
+				lb=insert(p);
+			}
+			return iterator(this,lb);
+		}
+	}
+	iterator find_wrapper(np p){
+		np lb=lower_bound(p);
+		if(cmp(lb->key,p->key)==cmp(p->key,lb->key))
+			return iterator(this,lb);
+		else return iterator(this,nil);
+	}
+	iterator count_wrapper(auto&&key){
+		if constexpr(allow_duplicate_keys){
+			node P(forward(key),val_t(),0,nil,nil,nil);
+			size_t R=order_of_key(lower_bound(&P));
+			P->time=UINT64_MAX;
+			return R-order_of_key(lower_bound(&P));
+		}
+		return nil!=find(key);
 	}
 	public:
 	// fundamental manipulation
-	master_rbtree():cur_size(0),time(0){
+	rbtree():cur_size(0),time(0){
 		nil=new node(nullptr);
-		bs=new node(nil);
-		es=new node(nil);
-		bs->pr=bs,bs->nx=es;
-		es->pr=bs,es->nx=es;
 		root=nil;
 	}
 	// access
-	iterator find(const key_t&key){}
-	iterator lower_bound(const key_t&key){}
-	iterator upper_bound(const key_t&key){}
-	iterator find_by_order(int64_t idx){}
-	size_t order_of_key(const key_t&key){}
-	const val_t&operator[](const key_t&key){}
-	size_t count(const key_t&key){}
-	size_t contains(const key_t&key){}
+	iterator find(const key_t&key){
+		node tar(key,val_t(),0,nil,nil,nil);
+		return find_wrapper(&tar);
+	}
+	iterator find(key_t&&key){
+		node tar(move(key),val_t(),0,nil,nil,nil);
+		return find_wrapper(&tar);
+	}
+	iterator lower_bound(const key_t&key){
+		node tar(key,val_t(),0,nil,nil,nil);
+		return iterator(this,lower_bound(&tar));
+	}
+	iterator lower_bound(key_t&&key){
+		node tar(move(key),val_t(),0,nil,nil,nil);
+		return iterator(this,lower_bound(&tar));
+	}
+	iterator upper_bound(const key_t&key){
+		node tar(key,val_t(),UINT64_MAX,nil,nil,nil);
+		return iterator(this,lower_bound(&tar));
+	}
+	iterator upper_bound(key_t&&key){
+		node tar(move(key),val_t(),UINT64_MAX,nil,nil,nil);
+		return iterator(this,lower_bound(&tar));
+	}
+	iterator find_by_order(int64_t idx){
+		if(idx>=cur_size||-idx>cur_size)return iterator(this,nil);
+		return iterator(this,ordered_access(idx>=0?idx:cur_size+idx));
+	}
+	size_t order_of_key(const key_t&key){
+		node tar(key,val_t(),0,nil,nil,nil);
+		return order_of_node(lower_bound(&tar));
+	}
+	size_t order_of_key(key_t&&key){
+		node tar(move(key),val_t(),0,nil,nil,nil);
+		return order_of_node(lower_bound(&tar));
+	}
+	size_t count(const key_t&key){ return find_wrapper(key); }
+	size_t count(key_t&&key){ return find_wrapper(move(key)); }
+	bool contains(const key_t&key){ return nil!=find(key); }
 	// change
-	iterator insert(value_type&&x){}
-	iterator emplace(key_t&&key,val_t&&val){}
-	iterator erase(const key_t&key){}
-	iterator erase(iterator&itr){}
+	iterator insert(value_type&&val){return insert_wrapper(move(val));}
+	iterator insert(const value_type&val){return insert_wrapper(val);}
+	iterator insert(const key_t&key,const val_t&val){return insert_wrapper(vp(key,val));}
+	iterator insert(const key_t&key,val_t&&val){return insert_wrapper(vp(key,move(val)));}
+	iterator insert(key_t&&key,const val_t&val){return insert_wrapper(vp(move(key),val));}
+	iterator insert(key_t&&key,val_t&&val){return insert_wrapper(vp(move(key),move(val)));}
+	iterator erase(iterator&itr){
+		if(itr!=nil){
+			cur_size-=1;
+			np nx=next(itr.n);
+			erase(itr.n);
+			return itr.n=nx;
+		}
+		return iterator(this,nil);
+	}
+	iterator erase(iterator&&itr){ return erase(itr); }
+	iterator erase(const key_t&key){ return erase(find(key)); }
+	iterator erase(key_t&&key){ return erase(find(move(key))); }
 	// memory
-	void clear(){}
-	bool empty(){}
-	size_t size(){}
+	void clear(){
+		if(root==nil)return;
+		time=0;
+		vector<np>st;
+		np c=minimum(root);
+		while(c!=nil){
+			st.push_back(c);
+			c=next(c);
+		}
+		for(auto&x:st)delete x;
+	}
+	bool empty(){return cur_size==0;}
+	size_t size(){return cur_size;}
 	// iterator
-	iterator begin(){}
-	iterator end(){}
-	iterator rbegin(){}
-	iterator rend(){}
-protected:
-	val_t&operator[](const key_t&key){}
-	val_t prod(const key_t&l,const key_t&r){}
-	void apply(const key_t&l,const key_t&r,const lazy_t&lazy){}
+	iterator begin(){
+		if(cur_size) return iterator(this,minimum(root));
+		return iterator(this,nil);
+	}
+	iterator end(){ return iterator(this,nil); }
+	reverse_iterator rbegin(){
+		if(cur_size) return reverse_iterator(this,maximum(root));
+		return iterator(this,nil);
+	}
+	reverse_iterator rend(){ return reverse_iterator(this,nil); }
+};
+
+template<class key_t,class cmp=less<key_t>,bool allow_duplicate_keys=true>
+class multiset:public rbtree<key_t,null_t,cmp,allow_duplicate_keys>{
+	using super=rbtree<key_t,null_t,cmp,allow_duplicate_keys>;
+	public:
+	using iterator=typename super::iterator;
+	const key_t&operator[](int64_t idx){ return *super::find_by_order(idx); }
+	iterator insert(const key_t&key){ return super::insert(key,null_t()); }
+	iterator insert(key_t&&key){ return super::insert(move(key),null_t()); }
 };
 
 template<class key_t,class cmp=less<key_t>>
-class multiset:public master_rbtree<key_t,null_t,null_t,null_t,null_t,null_t,null_t,null_t,cmp,true>{};
-
-template<class key_t,class cmp=less<key_t>>
-class set:public master_rbtree<key_t,null_t,null_t,null_t,null_t,null_t,null_t,null_t,cmp,false>{};
+using set=multiset<key_t,cmp,false>;
 
 template<class key_t,class val_t,class cmp=less<key_t>>
-class multimap:public master_rbtree<key_t,val_t,null_t,null_t,null_t,null_t,null_t,null_t,cmp,true>{};
+using multimap=rbtree<key_t,val_t,cmp,true>;
 
 template<class key_t,class val_t,class cmp=less<key_t>>
-class map:public master_rbtree<key_t,val_t,null_t,null_t,null_t,null_t,null_t,null_t,cmp,false>{};
-
-template<class key_t,class val_t,class op,class e,class cmp=less<key_t>>
-class multimap_monoid:public master_rbtree<key_t,val_t,op,e,null_t,null_t,null_t,null_t,cmp,true>{};
-
-template<class key_t,class val_t,class op,class e,class cmp=less<key_t>>
-class map_monoid:public master_rbtree<key_t,val_t,op,e,null_t,null_t,null_t,null_t,cmp,false>{};
-
-template<class key_t,class val_t,class op,class e,class lazy_t,class mapping,class composition,class id,class cmp=less<key_t>>
-class lazymultimap:public master_rbtree<key_t,val_t,op,e,lazy_t,mapping,composition,id,cmp,true>{};
-
-template<class key_t,class val_t,class op,class e,class lazy_t,class mapping,class composition,class id,class cmp=less<key_t>>
-class lazymap:public master_rbtree<key_t,val_t,op,e,lazy_t,mapping,composition,id,cmp,false>{};
+class map:public rbtree<key_t,val_t,cmp,false>{
+	public:
+	using iterator=typename rbtree<key_t,val_t,cmp,false>::iterator;
+	val_t&operator[](const key_t&key){
+		iterator p=this->find(key);
+		if(p.n!=this->nil)return p.n->val;
+		return this->emplace(key,val_t()).n->val;
+	}
+};
 
 }
 #endif
