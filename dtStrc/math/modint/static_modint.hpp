521728a41bfbddf467491887ee43d1cc43a89b71
CleverElsie <yubisiba@gmail.com>
2025-03-22 16:44:43 +0900

    fix modint

--- dtStrc/math/modint/static_modint.hpp
+++ dtStrc/math/modint/static_modint.hpp
@@ -9,11 +9,28 @@ namespace elsie{
     using namespace std;
     template<uint32_t M=998244353>
     class mint{
-        private: int32_t x;
+        public: // static
+        constexpr static size_t mod(){ return M; }
+        template<integral T>
+        static mint raw(T val){
+            mint r;
+            r.x=val;
+            return r;
+        }
+        private: uint32_t x; // field
+        template<class T>
+        concept ints=is_integral_v<T>||same_as<T,__int128_t>||same_as<T,__uint128_t>;
         public:
         mint():x(0){}
         mint(const mint&n):x(n.x){}
-        template<integral T>mint(T val){ x=val%M+(val<0)*M; }
+        template<ints T>mint(T val){
+            if constexpr(is_signed_v<T>||same_as<T,__int128_t>){
+                T v=val%M;
+                if(v<0)v+=M;
+                x=v;
+            }else x=val%M;
+        }
+
         uint32_t val()const{ return x; }
         mint pow(uint64_t n)const{
             mint v=*this,r=1;
@@ -31,43 +48,68 @@ namespace elsie{
             }
             return mint(v);
         }
-        size_t mod(){ return M; }
-        template<integral T>friend mint raw(T val){
-            mint r;
-            r.x=val;
-            return r;
-        }
 
-        mint&operator=(const mint&a)&{x=a.x;return*this;}
-        template<integral T>mint&operator=(const T&a)&{return*this=mint(a);}
+        mint&operator=(const mint&a){x=a.x;return*this;}
+        template<ints T>mint&operator=(const T&a){return*this=mint(a);}
         mint operator+(){return*this;}
-        mint operator-(){return mint(-x);}
-        mint&operator++(){if(++x==M)x=0;return*this;}
-        mint&operator--(){if(--x==-1)x=M-1;return*this;}
+        mint operator-(){return mint(M-x);}
+        mint&operator++(){if(x++==M-1)x=0;return*this;}
+        mint&operator--(){if(x--==0)x=M-1;return*this;}
         mint operator++(int32_t){mint r=*this;++*this;return r;}
         mint operator--(int32_t){mint r=*this;--*this;return r;}
-        mint&operator+=(const mint&a){x+=a.x;if(x>=M)x-=M;return*this;}
-        mint&operator-=(const mint&a){x-=a.x;if(x<0)x+=M;return*this;}
-        mint&operator*=(const mint&a){x=x*a.x%M;return*this;}
-        mint&operator/=(const mint&a){return*this*=a.inv();}
-        template<integral T>mint&operator+=(const T&a){return*this+=mint(a);}
-        template<integral T>mint&operator-=(const T&a){return*this-=mint(a);}
-        template<integral T>mint&operator*=(const T&a){return*this*=mint(a);}
-        template<integral T>mint&operator/=(const T&a){return*this*=mint(a).inv();}
-        template<class T>friend mint operator+(const mint&a,const T&b){return mint(a.x)+=b;}
-        template<class T>friend mint operator-(const mint&a,const T&b){return mint(a.x)-=b;}
-        template<class T>friend mint operator*(const mint&a,const T&b){return mint(a.x)*=b;}
-        template<class T>friend mint operator/(const mint&a,const T&b){return mint(a.x)/=b;}
+        template<class T>
+        mint&operator+=(T a) requires is_constructible_v<mint,T> {
+            if constexpr(same_as<T,mint<M>>) x=(u64)x+a.x-((u64)x+a.x>=M?M:0);
+            else *this+=mint(a);
+            return*this;
+        }
+        template<class T>
+        mint&operator-=(T a) requires is_constructible_v<mint,T> {
+            if constexpr(same_as<T,mint<M>>) x=(x>=a.x?x-a.x:x+(M-a.x));
+            else*this-=mint(a);
+            return*this;
+        }
+        template<class T>
+        mint&operator*=(T a) requires is_constructible_v<mint,T> {
+            if constexpr(same_as<T,mint<M>>) x=x*a.x%M;
+            else*this*=mint(a);
+            return*this;
+        }
+        template<class T>
+        mint&operator/=(mint a) requires is_constructible_v<mint,T> {
+            if constexpr(same_as<T,mint<M>>) return*this*=a.inv();
+            else return*this*=mint(a).inv();
+        }
+        template<class T>friend mint operator+(mint a,T b) requires is_constructible_v<mint,T> {
+            if constexpr(same_as<T,mint>) return a+=b;
+            else return a+=mint(b);
+        }
+        template<class T>friend mint operator-(mint a,T b) requires is_constructible_v<mint,T> {
+            if constexpr(same_as<T,mint>) return a-=b;
+            else return a-=mint(b);
+        }
+        template<class T>friend mint operator*(mint a,T b) requires is_constructible_v<mint,T> {
+            if constexpr(same_as<T,mint>) return a*=b;
+            else return a*=mint(b);
+        }
+        template<class T>friend mint operator/(mint a,T b) requires is_constructible_v<mint,T> {
+            if constexpr(same_as<T,mint>) return a*=b.inv();
+            return a*=mint(b).inv();
+        }
+        template<ints T>friend mint operator+(T a,mint b){ return b*=mint(a); }
+        template<ints T>friend mint operator-(T a,mint b){ return b-=mint(a); }
+        template<ints T>friend mint operator*(T a,mint b){ return b*=mint(a); }
+        template<ints T>friend mint operator/(T a,mint b){ return b*=mint(a).inv(); }
 
-        friend bool operator==(const mint&a,const mint&b){return a.x==b.x;}
-        friend bool operator!=(const mint&a,const mint&b){return a.x!=b.x;}
-        template<integral T>friend bool operator==(const mint&a,const T&b){return a.x==mint(b);}
-        template<integral T>friend bool operator!=(const mint&a,const T&b){return a.x!=mint(b);}
-        template<integral T>friend bool operator==(const T&a,const mint&b){return mint(a)==b.x;}
-        template<integral T>friend bool operator!=(const T&a,const mint&b){return mint(a)!=b.x;}
+        friend bool operator==(mint a,mint b){return a.x==b.x;}
+        friend bool operator!=(mint a,mint b){return a.x!=b.x;}
+        template<ints T>friend bool operator==(mint a,T b){return a.x==mint(b).x;}
+        template<ints T>friend bool operator!=(mint a,T b){return a.x!=mint(b).x;}
+        template<ints T>friend bool operator==(T a,mint b){return mint(a).x==b.x;}
+        template<ints T>friend bool operator!=(T a,mint b){return mint(a).x!=b.x;}
 
-        friend ostream&operator<<(ostream&os,const mint&a){return os<<a.x;}
-        friend istream&operator>>(istream&is,const mint&a){
+        friend ostream&operator<<(ostream&os,mint a){return os<<a.x;}
+        friend istream&operator>>(istream&is,mint&a){
             int64_t v;is>>v;
             a=mint(v);
             return is;
