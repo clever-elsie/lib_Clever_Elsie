d0e4af1f6ba431f25636f18fbcd5e6d0c3c26f16
CleverElsie <yubisiba@gmail.com>
2025-05-07 13:53:09 +0900

    x5.0 faster arbitrary mod convolution

--- math/convolution.hpp
+++ math/convolution.hpp
@@ -1,16 +1,18 @@
 #ifndef ELSIE_CONVOLUTION
 #define ELSIE_CONVOLUTION
 #include <chrono>
-#include<iostream>
+#include <iostream>
 #include <array>
 #include <vector>
 #include <cstdint>
 #include <variant>
 #include <algorithm>
 #include <type_traits>
+#include <utility>
 #include <math/prime_factor.hpp>
 namespace elsie{
 using namespace std;
+
 template<uint32_t M=998244353>
 class convolution_fast{
 static_assert(is_prime(M),"convolution_fast requires that M is prime");
@@ -70,29 +72,29 @@ void ntt(vector<u32>&a){
       if(s+1!=(1u<<len)) rot=(u64)rot*info.r2[countr_zero(~s)]%M;
     }
     ++len;
-  }else{
-    u32 rot=1,imag=info.r[2],p=1u<<(h-len-2);
-    for(u32 s=0;s<(1u<<len);++s){
-      u32 rot2=(u64)rot*rot%M,rot3=(u64)rot2*rot%M;
-      u32 offs=s<<(h-len);
-      for(u32 i=0;i<p;++i){
-        u32 a0=a[i+offs],a1=(u64)a[i+offs+p]*rot%M;
-        u32 a2=(u64)a[i+offs+2*p]*rot2%M;
-        u32 a3=(u64)a[i+offs+3*p]*rot3%M;
-        u32 a1_a3imag=u64(a1>=a3?a1-a3:(M-a3)+a1)*imag%M;
-        u32 a02=(u64)a0+a2-((u64)a0+a2>=M?M:0);
-        u32 a13=(u64)a1+a3-((u64)a1+a3>=M?M:0);
-        u32 a0n2=(a0>=a2?a0-a2:(M-a2)+a0);
-        a[i+offs]=(u64)a02+a13-((u64)a02+a13>=M?M:0);
-        a[i+offs+p]=(a02>=a13?a02-a13:(M-a13)+a02);
-        a[i+offs+2*p]=(u64)a0n2+a1_a3imag-((u64)a0n2+a1_a3imag>=M?M:0);
-        a[i+offs+3*p]=(a0n2>=a1_a3imag?a0n2-a1_a3imag:(M-a1_a3imag)+a0n2);
+    }else{
+      u32 rot=1,imag=info.r[2],p=1u<<(h-len-2);
+      for(u32 s=0;s<(1u<<len);++s){
+        u32 rot2=(u64)rot*rot%M,rot3=(u64)rot2*rot%M;
+        u32 offs=s<<(h-len);
+        for(u32 i=0;i<p;++i){
+          u32 a0=a[i+offs],a1=(u64)a[i+offs+p]*rot%M;
+          u32 a2=(u64)a[i+offs+2*p]*rot2%M;
+          u32 a3=(u64)a[i+offs+3*p]*rot3%M;
+          u32 a1_a3imag=u64(a1>=a3?a1-a3:(M-a3)+a1)*imag%M;
+          u32 a02=(u64)a0+a2-((u64)a0+a2>=M?M:0);
+          u32 a13=(u64)a1+a3-((u64)a1+a3>=M?M:0);
+          u32 a0n2=(a0>=a2?a0-a2:(M-a2)+a0);
+          a[i+offs]=(u64)a02+a13-((u64)a02+a13>=M?M:0);
+          a[i+offs+p]=(a02>=a13?a02-a13:(M-a13)+a02);
+          a[i+offs+2*p]=(u64)a0n2+a1_a3imag-((u64)a0n2+a1_a3imag>=M?M:0);
+          a[i+offs+3*p]=(a0n2>=a1_a3imag?a0n2-a1_a3imag:(M-a1_a3imag)+a0n2);
+        }
+        if(s+1!=(1u<<len))
+          rot=(u64)rot*info.r3[countr_zero(~s)]%M;
       }
-      if(s+1!=(1u<<len))
-        rot=(u64)rot*info.r3[countr_zero(~s)]%M;
+      len+=2;
     }
-    len+=2;
-  }
   }
 }
 void ntt_inv(vector<u32>&a){
@@ -173,89 +175,302 @@ template<bool is_Zmod=true>
 vector<u32>prod(vector<u32>a,vector<u32>b){ return prod_move<is_Zmod>(move(a),move(b)); }
 };
 
-template<class T,T M=998244353,size_t psz=2>
+template<size_t M=998244353,size_t mod_cnt=5>
 class convolution{
-  static_assert(1<=psz&&psz<=11,"Even if you use a good prime as M, you still need to satisfy the condition 1<=psz<=11.");
-  static constexpr bool f_prime=is_prime<T>(M);
-  using u32 = uint32_t;
-  static constexpr array<u32,11>mods{
-    2'885'681'153u/*43 * 2^26 +1*/,2'717'908'993u/*81 * 2^25 +1*/,
-    2'483'027'969u/*37 * 2^26 +1*/,2'281'701'377u/*17 * 2^27 +1*/,
-    2'113'929'217u/*63 * 2^25 +1*/,2'013'265'921u/*15 * 2^27 +1*/,
-    1'811'939'329u/*27 * 2^26 +1*/,1'711'276'033u/*51 * 2^25 +1*/,
-    1'107'296'257u/*33 * 2^25 +1*/,  469'762'049u/* 7 * 2^26 +1*/,
+  using i32=int32_t; using u32=uint32_t;
+  using i64=int64_t; using u64=uint64_t;
+  static constexpr size_t max_mods=11;
+  static_assert(mod_cnt<=5);
+  static constexpr array<u32,max_mods>mods_list{
+    2'717'908'993u/*81 * 2^25 +1*/, 2'113'929'217u/*63 * 2^25 +1*/,
+    1'711'276'033u/*51 * 2^25 +1*/, 1'107'296'257u/*33 * 2^25 +1*/,
       167'772'161u/* 5 * 2^25 +1*/
   };
-  template<size_t... I>
-  static constexpr array<T,psz+1>make_mods_s(index_sequence<I...>){ return{{mods[I]...,M}}; }
-  static constexpr array<T,psz+1>mods_s=make_mods_s(make_index_sequence<psz>{});
-  T garner_impl(const array<u32,psz>&b)const
-  requires is_integral_v<T>||same_as<T,__int128_t>||same_as<T,__uint128_t>{
-    using int_s=conditional_t<(sizeof(T)>8),__int128_t,int64_t>;
-    using vc=vector<int_s>;
-    constexpr __uint128_t threshold=__uint128_t(1)<<64;
-    array<int_s,psz+1>coeffs,constants;
-    coeffs.fill(1); constants.fill(0);
-    if constexpr(M<=threshold){
-      for(size_t k=0;k<b.size();++k){
-        int_s tmp=b[k]>=constants[k]?b[k]-constants[k]:((int_s)b[k]+mods_s[k])-constants[k];
-        int_s t=tmp*mod_inv<int_s>(coeffs[k],mods_s[k])%mods_s[k];
-        for(size_t i=k+1;i<=b.size();++i){
-          constants[i]=(t*coeffs[i]%mods_s[i]+constants[i])%mods_s[i];
-          coeffs[i]=coeffs[i]*mods_s[k]%mods_s[i];
-        }
-      }
-    }else{
-      for(size_t k=0;k<b.size();++k){
-        int_s tmp=b[k]>=constants[k]?b[k]-constants[k]:(b[k]+mods_s[k])-constants[k];
-        int_s t=tmp*mod_inv<int_s>(coeffs[k],mods_s[k])%mods_s[k];
-        for(size_t i=k+1;i<=b.size();++i){
-          constants[i]=(constants[i]+rem256(mul128(t,coeffs[i]),mods_s[i]))%mods_s[i];
-          coeffs[i]=rem256(mul128(coeffs[i],mods_s[k]),mods_s[i]);
-        }
+  static constexpr array<u32,max_mods>primitive_root_list{ 1100, 206, 947, 199, 124 };
+  class mint_ntt{
+    friend convolution;
+    using i32=int32_t; using u32=uint32_t;
+    using i64=int64_t; using u64=uint64_t;
+    template<size_t... I> static constexpr array<u32,mod_cnt> make_mods(index_sequence<I...>){ return {mods_list[I]...}; }
+    private:
+    static constexpr array<u32,mod_cnt> mods=make_mods(make_index_sequence<mod_cnt>{});
+    array<u32,mod_cnt>val;
+
+    template<size_t... I> constexpr mint_ntt& add_impl(const mint_ntt&rhs,index_sequence<I...>){
+      (...,(val[I]=(static_cast<u64>(val[I])+rhs.val[I])%mods[I]));
+      return*this;
+    }
+    template<size_t... I> constexpr mint_ntt& sub_impl(const mint_ntt&rhs,index_sequence<I...>){
+      (...,(val[I]=(static_cast<u64>(val[I])+mods[I]-rhs.val[I])%mods[I]));
+      return*this;
+    }
+    template<size_t... I> constexpr mint_ntt& mul_impl(const mint_ntt&rhs,index_sequence<I...>){
+      (...,(val[I]=static_cast<u32>(static_cast<u64>(val[I])*rhs.val[I]%mods[I])));
+      return*this;
+    }
+    template<size_t... I> constexpr mint_ntt& div_impl(const mint_ntt&rhs,index_sequence<I...>){
+      (...,(val[I]=static_cast<u32>(static_cast<u64>(val[I])*modinv<I>(rhs.val[I])%mods[I])));
+      return*this;
+    }
+    template<size_t... I> static constexpr mint_ntt negate_impl(const mint_ntt&v,index_sequence<I...>){
+      mint_ntt res(v);
+      (...,(res.val[I]=(mods[I]-v.val[I])%mods[I]));
+      return res;
+    }
+    template<size_t... I> constexpr void assign(u64 x,index_sequence<I...>){
+      (...,(val[I]=x%mods[I]));
+    }
+    public:
+    ~mint_ntt()=default;
+    constexpr mint_ntt()=default;
+    constexpr mint_ntt(mint_ntt&&)=default;
+    constexpr mint_ntt(const mint_ntt&)=default;
+    constexpr mint_ntt(u64 x){ assign(x,make_index_sequence<mod_cnt>{}); }
+    constexpr mint_ntt& operator=(mint_ntt&&)=default;
+    constexpr mint_ntt& operator=(const mint_ntt&)=default;
+    constexpr mint_ntt& operator=(u64 x){
+      assign(x,make_index_sequence<mod_cnt>{});
+      return *this;
+    }
+
+    constexpr mint_ntt& operator+=(const mint_ntt&rhs){ return add_impl(rhs,make_index_sequence<mod_cnt>{}); }  
+    constexpr mint_ntt& operator-=(const mint_ntt&rhs){ return sub_impl(rhs,make_index_sequence<mod_cnt>{}); }
+    constexpr mint_ntt& operator*=(const mint_ntt&rhs){ return mul_impl(rhs,make_index_sequence<mod_cnt>{}); }
+    constexpr mint_ntt& operator/=(const mint_ntt&rhs){ return div_impl(rhs,make_index_sequence<mod_cnt>{}); }
+
+    private:
+    template<size_t... k>
+    constexpr void outer_value(array<u64,mod_cnt+1>&coeffs,array<u64,mod_cnt+1>&constants,index_sequence<k...>)const{
+      ((outer_step<k>(coeffs,constants)),...);
+    }
+    template<i32 k>
+    constexpr void outer_step(array<u64,mod_cnt+1>&coeffs,array<u64,mod_cnt+1>&constants)const{
+      u64 tmp = val[k]>=constants[k]? val[k]-constants[k] : val[k]+mods[k]-constants[k];
+      u32 t = tmp*modinv<k>(coeffs[k])%mods[k];
+      inner_value<k>(coeffs,constants,t,make_index_sequence<mod_cnt-k-1>{});
+      if constexpr(M!=0){ // M==0 then assume M==2^64
+        constants[mod_cnt]=(t*coeffs[mod_cnt]%M+constants[mod_cnt])%M;
+        coeffs[mod_cnt]=coeffs[mod_cnt]*mods[k]%M;
+      }else{ // if M is 0, assume M = 2^64
+        constants[mod_cnt]=t*coeffs[mod_cnt]+constants[mod_cnt];
+        coeffs[mod_cnt]=coeffs[mod_cnt]*mods[k];
       }
     }
-    return constants.back();
+    template<i32 k,size_t... I>
+    constexpr void inner_value(array<u64,mod_cnt+1>&coeffs,array<u64,mod_cnt+1>&constants,const u32 t,index_sequence<I...>)const{
+      ((inner_step<k,I>(coeffs,constants,t)),...);
+    }
+    template<i32 k,i32 I>
+    constexpr void inner_step(array<u64,mod_cnt+1>&coeffs,array<u64,mod_cnt+1>&constants,const u32 t)const{
+      constexpr size_t i=k+1+I;
+      constants[i]=(t*coeffs[i]%mods[i]+constants[i])%mods[i];
+      coeffs[i]=coeffs[i]*mods[k]%mods[i];
+    }
+    public:
+    constexpr u64 value()const{
+      array<u64,mod_cnt+1>coeffs,constants;
+      coeffs.fill(1); constants.fill(0);
+      outer_value(coeffs,constants,make_index_sequence<mod_cnt>{});
+      return constants.back();
+    }
+    template<size_t I> static constexpr u32 modinv(u32 a){
+      i64 x=1,u=0,b=mods[I];
+      while(b){
+        i64 k=a/b,r=a%b,tmp=u;
+        a=b,b=r;
+        u=x-u*k;
+        x=tmp;
+      }
+      if(a<0)a=-a,x=-x;
+      if(a!=1)return -1;
+      return(x%mods[I]+mods[I])%mods[I];
+    }
+    template<size_t I> static constexpr u32 modpow(u32 a, u32 b){
+      u64 r=1,k=a;
+      for(;b;b>>=1){
+        if(b&1)r=r*k%mods[I];
+        k=k*k%mods[I];
+      }
+      return r;
+    }
+    private:
+    template<size_t... I> constexpr bool eq_impl(const mint_ntt&rhs,index_sequence<I...>)const{
+      return (...&&(val[I]==rhs.val[I]));
+    }
+    public:
+    friend constexpr bool operator!=(const mint_ntt&lhs,const mint_ntt&rhs){ return !lhs.eq_impl(rhs,make_index_sequence<mod_cnt>{}); }
+    friend constexpr bool operator==(const mint_ntt&lhs,const mint_ntt&rhs){ return lhs.eq_impl(rhs,make_index_sequence<mod_cnt>{}); }
+    friend constexpr mint_ntt operator+(const mint_ntt&lhs,const mint_ntt&rhs){return mint_ntt(lhs)+=rhs;}
+    friend constexpr mint_ntt operator-(const mint_ntt&lhs,const mint_ntt&rhs){return mint_ntt(lhs)-=rhs;}
+    friend constexpr mint_ntt operator*(const mint_ntt&lhs,const mint_ntt&rhs){return mint_ntt(lhs)*=rhs;}
+    friend constexpr mint_ntt operator/(const mint_ntt&lhs,const mint_ntt&rhs){return mint_ntt(lhs)/=rhs;}
+  };
+  struct ntt_info{
+    static constexpr i32 e2=25;
+    array<mint_ntt,e2+1>root,iroot;
+    array<mint_ntt,e2-1>root2,iroot2;
+    array<mint_ntt,e2-2>root3,iroot3;
+    private:
+    template<size_t... I>
+    constexpr void init_root(index_sequence<I...>){
+      (...,(root[e2].val[I]=mint_ntt::template modpow<I>(primitive_root_list[I],(convolution::mods_list[I]-1)>>e2)));
+      (...,(iroot[e2].val[I]=mint_ntt::template modinv<I>(root[e2].val[I])));
+    }
+    public:
+    constexpr ntt_info(){
+      init_root(make_index_sequence<mod_cnt>{});
+      for(i32 i=e2-1;i>=0;--i){
+        root[i]=root[i+1]*root[i+1];
+        iroot[i]=iroot[i+1]*iroot[i+1];
+      }
+      mint_ntt p(1),ip(1);
+      for(i32 i=0;i<=e2-2;++i){
+        root2[i]=root[i+2]*p;
+        iroot2[i]=iroot[i+2]*ip;
+        p*=iroot[i+2];
+        ip*=root[i+2];
+      }
+      p=ip=1;
+      for(i32 i=0;i<=e2-3;++i){
+        root3[i]=root[i+3]*p;
+        iroot3[i]=iroot[i+3]*ip;
+        p*=iroot[i+3];
+        ip*=root[i+3];
+      }
+    }
+  };
+  constexpr static ntt_info info=ntt_info();
+
+  void ntt(vector<mint_ntt>&a)const{
+    u32 len=0,n=static_cast<u32>(a.size()),h=countr_zero(n);
+    while(len<h){
+      if(h-len==1){
+        u32 p=1u<<(h-len-1);
+        mint_ntt rot(1);
+        for(u32 s=0;s<(1u<<len);++s){
+          for(u32 i=0,offs=s<<(h-len);i<p;++i){
+            mint_ntt l=a[i+offs],r=a[i+offs+p]*rot;
+            a[i+offs]=l+r;
+            a[i+offs+p]=l-r;
+          }
+          if(s+1!=(1u<<len)) rot*=info.root2[countr_zero(~s)];
+        }
+        ++len;
+      }else{
+        mint_ntt rot(1),imag(info.root[2]);
+        u32 p=1u<<(h-len-2);
+        for(u32 s=0;s<(1u<<len);++s){
+          mint_ntt rot2=rot*rot,rot3=rot2*rot;
+          for(u32 i=0,offs=s<<(h-len);i<p;++i){
+            mint_ntt&a0=a[i+offs];
+            mint_ntt a1=a[i+offs+p]*rot;
+            mint_ntt a2=a[i+offs+2*p]*rot2;
+            mint_ntt a3=a[i+offs+3*p]*rot3;
+            mint_ntt a1_a3imag=(a1-a3)*imag;
+            mint_ntt a02=a0+a2,a13=a1+a3;
+            mint_ntt a0n2=a0-a2;
+            a[i+offs]=a02+a13;
+            a[i+offs+p]=a02-a13;
+            a[i+offs+2*p]=a0n2+a1_a3imag;
+            a[i+offs+3*p]=a0n2-a1_a3imag;
+          }
+          if(s+1!=(1u<<len)) rot*=info.root3[countr_zero(~s)];
+        }
+        len+=2;
+      }
+    }
   }
-  template<u32 mod,bool is_Zmod> vector<u32>mod_vec(const vector<T>&a)const{
-    vector<u32>r(a.size());
-    if constexpr(is_Zmod){
-      for(auto rtr=r.begin(),atr=a.begin();atr!=a.end();++atr,++rtr)
-        *rtr=*atr;
-    }else{
-      for(auto rtr=r.begin(),atr=a.begin();atr!=a.end();++atr,++rtr){
-        T m=*atr%mod;
-        if constexpr(is_signed_v<T>||same_as<T,__int128_t>)
-          if(m<0)m+=mod;
-        *rtr=static_cast<u32>(m);
+  void inv_ntt(vector<mint_ntt>&a)const{
+    u32 n=static_cast<u32>(a.size()),h=countr_zero(n);
+    i32 len=h;
+    while(len){
+      if(len==1){
+        u32 p=1u<<(h-len);
+        mint_ntt rot(1);
+        for(u32 s=0;s<(1u<<len-1);++s){
+          for(u32 i=0,offs=s<<(h+1-len);i<p;++i){
+            mint_ntt l=a[i+offs];
+            mint_ntt&r=a[i+offs+p];
+            a[i+offs]+=r;
+            a[i+offs+p]=(l-r)*rot;
+          }
+          if(s+1!=(1u<<len-1)) rot*=info.iroot2[countr_zero(~s)];
+        }
+        --len;
+      }else{
+        u32 p=1u<<(h-len);
+        mint_ntt rot(1),imag(info.iroot[2]);
+        for(u32 s=0;s<(1u<<len-2);++s){
+          mint_ntt rot2(rot*rot),rot3(rot*rot2);
+          for(u32 i=0,offs=s<<(h+2-len);i<p;++i){
+            mint_ntt&a0=a[i+offs];
+            mint_ntt&a1=a[i+offs+p];
+            mint_ntt&a2=a[i+offs+2*p];
+            mint_ntt&a3=a[i+offs+3*p];
+            mint_ntt a2_a3imag=(a2-a3)*imag;
+            mint_ntt a01=a0+a1,a23=a2+a3;
+            mint_ntt a0n1=a0-a1;
+            a[i+offs]=a01+a23;
+            a[i+offs+p]=(a0n1+a2_a3imag)*rot;
+            a[i+offs+2*p]=(a01-a23)*rot2;
+            a[i+offs+3*p]=(a0n1-a2_a3imag)*rot3;
+          }
+          if(s+1!=(1u<<len-2))rot*=info.iroot3[countr_zero(~s)];
+        }
+        len-=2;
       }
     }
+  }
+
+  template<unsigned_integral T>
+  vector<uint64_t> convolution_naive(const vector<T>&a,const vector<T>&b)const{
+    size_t s=a.size()+b.size()-1;
+    vector<uint64_t> c(s,0);
+    const auto&p=(a.size()<b.size()?a:b);
+    const auto&q=(a.size()<b.size()?b:a);
+    for(size_t i=0;i<p.size();++i)
+      for(size_t j=0;j<q.size();++j)
+        if constexpr(M) c[i+j]=(c[i+j]+p[i]*q[j]%M)%M;
+        else c[i+j]+=p[i]*q[j];
+    return c;
+  }
+public:
+  using mint=mint_ntt;
+  convolution()=default;
+  template<unsigned_integral T>
+  vector<uint64_t> prod(const vector<T>&lhs,const vector<T>&rhs)const{
+    u32 n=lhs.size(),m=rhs.size();
+    u32 nm=n+m-1;
+    if(std::min(n,m)<=60) return convolution_naive(lhs,rhs);
+    u32 e=31-countl_zero(nm)+(popcount(nm)!=1);
+    u32 p=1u<<e;
+    vector<mint> a(p,0),b(p,0);
+    for(size_t i=0;i<lhs.size();++i) a[i]=lhs[i];
+    for(size_t i=0;i<rhs.size();++i) b[i]=rhs[i];
+    auto r=prod(std::move(a),std::move(b));
+    r.resize(nm);
     return r;
   }
-  template<bool is_Zmod,size_t... I>
-  vector<T>prod_impl(const vector<T>&a,const vector<T>&b,const size_t len,index_sequence<I...>)const{
-    vector<vector<u32>>r={convolution_fast<mods_s[I]>().prod_move(mod_vec<mods_s[I],is_Zmod>(a),mod_vec<mods[I],is_Zmod>(b))...};
-    vector<T>ret(len);
-    array<u32,psz>q;
-    for(size_t i=0;i<len;++i){
-      for(uint32_t j=0;j<psz;++j)
-        q[j]=r[j][i];
-      ret[i]=garner_impl(q);
-    }
-    return ret;
+  private:
+  template<size_t ...I>
+  mint_ntt calc_invk(size_t len,index_sequence<I...>)const{
+    mint_ntt v;
+    (...,(v.val[I]=mint_ntt::template modinv<I>(len)));
+    return v;
   }
   public:
-  convolution(){}
-  template<bool is_Zmod=false>
-  vector<T>prod(const vector<T>&a,const vector<T>&b)const{
-    const size_t len=a.size()+b.size()-1;
-    const size_t step=63-countl_zero(len)+(popcount(len)!=1);
-    size_t max_step=0;
-    if constexpr(sizeof(T)>8) for(T p=M-1;(p&1==0);p>>=1)++max_step;
-    else max_step=countr_zero(uint64_t(M)-1);
-    if constexpr(f_prime&&same_as<T,u32>)if(max_step>=step)
-      return convolution_fast<M>().template prod<is_Zmod>(a,b);
-    return prod_impl<is_Zmod>(a,b,len,make_index_sequence<psz>{});
+  vector<uint64_t> prod(vector<mint>&&lhs,vector<mint>&&rhs)const{
+    assert(lhs.size()==rhs.size() && popcount(lhs.size())==1);
+    ntt(lhs);
+    ntt(rhs);
+    for(size_t i=0;i<lhs.size();++i)
+      lhs[i]*=rhs[i];
+    inv_ntt(lhs);
+    vector<uint64_t>ret(lhs.size());
+    mint_ntt invk=calc_invk(lhs.size(),make_index_sequence<mod_cnt>{});
+    for(size_t i=0;i<lhs.size();++i)
+      ret[i]=(lhs[i]*=invk).value();
+    return ret;
   }
 };
 }
