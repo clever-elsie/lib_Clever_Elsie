0d986516eb36c7a09fd6ce9f866172df2f28de95
CleverElsie <yubisiba@gmail.com>
2025-07-20 21:26:18 +0900

    mexを汎化

--- .document/dtStrc/math/mex.md
+++ .document/dtStrc/math/mex.md
@@ -1,6 +1,73 @@
-### mex
-数列に含まれる最小の非負整数を求めるためのデータ構造  
-`add(int)`で追加し、`erase(int)`で削除、`find(int)`でmexを返す。  
-重複を考慮した区間 $[l,r)$ の管理をしているだけ。  
-計算量はすべての操作に対して $O(\lg N)$.  
-同じ数字の重複を許すため`add`した回数と同じ回数`erase`しないと消えない．
+# mex
+MEXとは，Minimum EXcluded valueの略であり，全順序集合の部分集合に含まれない最小の値のことである．
+
+このライブラリは全順序集合となる型`T`について，その部分集合を保持するクラス`mex`を定義する．  
+最小値を求めるため，自明な事実として全順序集合は下に有界でなければならない．  
+また値を求める必要があることから，有限集合または加算無限集合でなければならない．
+
+ライブラリでサポートしている型は`std::signed_integral`と`std::string`のみである．  
+`std::string`は英小文字のみを扱う．
+`std::less<std::string>`を使うと無限に後ろに`a`を追加するだけの自明なものになるので，同じ長さのときに次に早い辞書順比較で，短い順に並べる．
+
+## mex_traits
+```C++
+template<class T>
+struct mex_traits{
+  using value_type=T;
+  constexpr static T LOWER_BOUND;
+  constexpr static T LOWER_LIMIT;
+  constexpr static struct{
+    constexpr bool operator()(const T&lhs,const T&rhs)const;
+  } pred{}; // std::less<T>など
+  constexpr static T prev(const T&); // const T& または T
+  constexpr static T next(const T&);
+};
+```
+`LOWER_BOUND`はMEXに含まれない`T`の下界．  
+`LOWER_LIMIT`はMEXに含むことができる`T`の下限．  
+`pred`は`invokable`は比較述語で，`operator<`と同じように`pred(a,a)`が`false`になるべき．`pred`によって全順序になる必要がある．  
+`prev`は整数におけるデクリメントの役割．  
+`next`は整数におけるインクリメントの役割．  
+`prev(LOWER_BOUND,LOWER_LIMIT)`である必要がある．  
+`LOWER_BOUND==prev(LOWER_LIMIT)`である必要がある．  
+`next(LOWER_BOUND)==LOWER_LIMIT`である必要がある．
+
+## コンストラクタ/デストラクタ/operator=
+```C++
+mex<T,traits=mex_traits<T>>();
+mex(std::initializer_list<T>);
+mex(mex&&)=default;
+mex(const mex&)=default;
+~mex()=default;
+mex& operator=(mex&&)=default;
+mex& operator=(const mex&)=default;
+```
+`mex_traits`は上記の性質を満たす構造体を渡すことができる．
+
+## 追加
+```C++
+void add(const T&value);
+```
+計算量 $O(\lg N)$ . $N$ はそのとき集合に含まれる相異なる要素数．  
+集合に要素を加える．
+
+## 削除
+```C++
+void erase(const T&value);
+```
+計算量 $O(\lg N)$ . $N$ はそのとき集合に含まれる相異なる要素数．  
+集合から`value`を削除する．重複している場合は $1$ 減らす．
+
+## mex
+```C++
+const T& find()const;
+```
+計算量 $O(1)$ .  
+そのときのMEXを返す．
+
+## 要素数
+```C++
+size_t count(const T&value)const;
+```
+計算量 $O(1)$ .  
+指示された要素の要素数を求める．
--- dtStrc/math/mex.hpp
+++ dtStrc/math/mex.hpp
@@ -3,60 +3,143 @@
 #include <map>
 #include <cstddef>
 #include <unordered_map>
+#include <type_traits>
+#include <string>
+#include <cassert>
+#include <initializer_list>
 namespace elsie{
-  using namespace std;
-  struct mex{
-    map<int,int>v; // [l,r)
-    unordered_map<int,int>c; // {val,cnt}
-    mex(){v[-1]=0;}
-    void add(int a){
-      { // count phase
-        auto itr=c.find(a);
-        if(itr!=c.end()){
-          itr->second++;
-          return;
-        }else c[a]=1;
-      }
-      if(a<0)return;
-      auto itr=v.upper_bound(a);
-      itr--;
-      if(a<itr->second)return;
-      if(itr->second==a) itr->second++;
+
+template<class T>
+struct mex_traits;
+
+template<class traits>
+concept MEX_TRAITS=requires(traits x){
+  typename traits::value_type;
+  {traits::LOWER_BOUND}->std::convertible_to<typename traits::value_type>;
+  {traits::LOWER_LIMIT}->std::convertible_to<typename traits::value_type>;
+  {traits::pred}->std::invocable<typename traits::value_type,typename traits::value_type>;
+  {traits::prev(traits::LOWER_LIMIT)}->std::convertible_to<typename traits::value_type>;
+  {traits::next(traits::LOWER_BOUND)}->std::convertible_to<typename traits::value_type>;
+};
+
+
+template<std::signed_integral INT>
+struct mex_traits<INT>{
+  using value_type=INT;
+  constexpr static INT LOWER_BOUND=-1;
+  constexpr static INT LOWER_LIMIT=0;
+  constexpr static std::less<INT> pred{};
+  constexpr static INT prev(INT x){ return x-1; }
+  constexpr static INT next(INT x){ return x+1; }
+};
+
+template<>
+struct mex_traits<std::string>{
+  using value_type=std::string;
+  constexpr static std::string LOWER_BOUND="";
+  constexpr static std::string LOWER_LIMIT="a";
+  constexpr static struct{
+    constexpr bool operator()(const std::string&lhs,const std::string&rhs)const{
+      return lhs.size()!=rhs.size()?lhs.size()<rhs.size():lhs<rhs;
+    }
+  } pred{};
+  constexpr static std::string prev(const std::string&v){
+    assert(v.size());
+    std::string ret(v);
+    ssize_t i=v.size()-1;
+    for(;i>=0;--i){
+      if(ret[i]=='a') ret[i]='z';
       else{
-        v[a]=a+1;
-        itr=v.find(a);
-      }
-      auto jtr=itr;
-      jtr++;
-      if(jtr==v.end())return;
-      if(itr->second==jtr->first){
-        itr->second=jtr->second;
-        v.erase(jtr);
+        --ret[i];
+        break;
       }
     }
-    void erase(int a){
-      { // count phase
-        auto itr=c.find(a);
-        if(itr==c.end())return;
-        itr->second--;
-        if(itr->second>0) return;
-        c.erase(itr);
+    if(i==-1) ret.pop_back();
+    return ret;
+  }
+  constexpr static std::string next(const std::string&v){
+    std::string ret(v);
+    size_t i=0;
+    for(;i<ret.size();++i){
+      if(ret[i]=='z') ret[i]='a';
+      else{
+        ++ret[i];
+        break;
       }
-      if(a<0)return;
-      auto itr=v.upper_bound(a);
-      itr--;
-      if(itr->second-1==a){
-        itr->second--;
+    }
+    if(i==ret.size()) ret.push_back('a');
+    return ret;
+  }
+};
+
+
+template<class T,class hash=std::hash<T>,class traits=mex_traits<T>>
+requires MEX_TRAITS<traits>
+struct mex{
+  static_assert(traits::pred(traits::LOWER_BOUND,traits::LOWER_LIMIT));
+  static_assert(traits::LOWER_BOUND==traits::prev(traits::LOWER_LIMIT));
+  static_assert(traits::next(traits::LOWER_BOUND)==traits::LOWER_LIMIT);
+  std::map<T,T,decltype(traits::pred)>v; // [l,r)
+  std::unordered_map<T,size_t,hash>c; // {val,cnt}
+  mex():v(traits::pred),c(){
+    v[traits::LOWER_BOUND]=traits::next(traits::LOWER_BOUND);
+  }
+  mex(std::initializer_list<T>init):mex(){
+    for(auto itr:init) add(itr);
+  }
+  mex(mex&&)=default;
+  mex(const mex&)=default;
+  ~mex()=default;
+  mex& operator=(mex&&)=default;
+  mex& operator=(const mex&)=default;
+  void add(const T&value){
+    { // count phase
+      if(auto itr=c.find(value);itr!=c.end()){
+        ++(itr->second);
         return;
-      }
-      v[a+1]=itr->second;
-      itr->second=a;
+      }else c[value]=1;
     }
-    int find(){return v[-1];}
-    size_t count(int a){
-      auto itr=c.find(a);
-      return(itr==c.end()?0ull:itr->second);
+    if(traits::pred(value,traits::LOWER_LIMIT))return;
+    auto itr=v.upper_bound(value);
+    --itr;
+    if(traits::pred(value,itr->second))return;
+    if(itr->second==value) itr->second=traits::next(itr->second);
+    else{
+      v[value]=traits::next(value);
+      itr=v.find(value);
     }
-  };
+    auto jtr=itr;
+    ++jtr;
+    if(jtr==v.end())return;
+    if(itr->second==jtr->first){
+      itr->second=jtr->second;
+      v.erase(jtr);
+    }
+  }
+  void erase(const T&value){
+    { // count phase
+      auto itr=c.find(value);
+      if(itr==c.end())return;
+      --(itr->second);
+      if(itr->second>0) return;
+      c.erase(itr);
+    }
+    if(traits::pred(value,traits::LOWER_LIMIT))return;
+    auto itr=v.upper_bound(value);
+    --itr;
+    if(itr->second==traits::next(value)){
+      itr->second=traits::prev(itr->second);
+      return;
+    }
+    v[traits::next(value)]=itr->second;
+    itr->second=value;
+  }
+  const T& find()const{return v.begin()->second;}
+  size_t count(const T&a){
+    auto itr=c.find(a);
+    return(itr==c.end()?0ull:itr->second);
+  }
+};
+
 }
 #endif
